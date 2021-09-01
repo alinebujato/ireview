621656ab6a6ec20247a267b33d407cc2f0142c61
Aline Bujato <aline_bujato@hotmail.com>
2021-08-31 21:32:23 -0300

    styled component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const MeuBotao = styled.button`
+    background: #0099FF;
+    border: 2px solid #0099FF;
+    border-radius: 3px;
+    color: #FDFDFD;
+    font-size: 1.2rem;
+    margin: 1rem;
+    padding: 1rem 1.5rem;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MeuBotao>banana</MeuBotao>
       </header>
     </div>
   );
